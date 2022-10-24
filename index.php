8d98194255f198466f8ed38d094f7661b48b3c20
Muhamad Gunandi <gunandindi@gmail.com>
2022-10-24 21:44:43 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+    echo "Hello World!<br>";
+
+    $nama = "Muhamad Gunandi";
+    $umur = 21;
+
+    echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+    $namaAdik = "Jono";
+    $umurAdik = "18";
+
+    echo "Nama adik saya <strong>$namaAdik</strong>, saya berusia $umurAdik tahun.<br>";
+
+    $selisihUmur = $umur - $umurAdik;
+    echo "selisih usia saya dan adik saya adalah $selisihUmur tahun."
+
+?>
